119aa8b206366949383e90214ff513b398971619
Paurakh Pyakurel <ppaurakh0099@gmail.com>
2026-01-27 22:03:01 +0545

    fix(settings): use compact inline inputs for dropdowns (font size, auto-lock, reminder time)

---
+++ Reflecta/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Reflecta;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Reflecta/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Reflecta;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
